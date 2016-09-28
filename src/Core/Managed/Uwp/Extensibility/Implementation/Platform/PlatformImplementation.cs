bc7d63a1db49d7ebac911855c57223a6363dfc2b
Jacek Blaszczynski <jacek.blaszczynski@outlook.com>
2016-09-28 22:05:01 +0200

    Implemented independent  Uwp projects for Core.SDK, ApplicationInsightsTypes, TestFramework and UWP.Tests

---
+++ src/Core/Managed/Uwp/Extensibility/Implementation/Platform/PlatformImplementation.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Platform
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation.External;
+
+    internal class PlatformImplementation : IPlatform
+    {
+        private IDebugOutput debugOutput = null;
+        
+        public IDictionary<string, object> GetApplicationSettings()
+        {
+            return null;
+        }
+
+        public string ReadConfigurationXml()
+        {
+            return null;
+        }
+
+        public ExceptionDetails GetExceptionDetails(Exception exception, ExceptionDetails parentExceptionDetails)
+        {
+            return ExceptionConverter.ConvertToExceptionDetails(exception, parentExceptionDetails);
+        }
+
+        /// <summary>
+        /// Returns the platform specific Debugger writer to the VS output console.
+        /// </summary>
+        public IDebugOutput GetDebugOutput()
+        {
+            if (this.debugOutput == null)
+            {
+                this.debugOutput = new TelemetryDebugWriter(); 
+            }
+            
+            return this.debugOutput;
+        }
+    }
+}
